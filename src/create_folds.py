2cf96b1bb829ad446288cce1644c4b174b5c0aa5
Marc <35631594+malbernhe@users.noreply.github.com>
2020-05-11 14:36:37 +0200

    .

--- src/create_folds.py
+++ src/create_folds.py
@@ -8,14 +8,15 @@ if __name__ == "__main__":
     df["kfold"] = -1
     df = df.sample(frac=1).reset_index(drop=True)
 
+    # k-fold cross-validation
     kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=42)
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
-        print (len(train_idx), len(val_idx))
+        #print (len(train_idx), len(val_idx))
         df.loc[val_idx, 'kfold'] = fold
 
-    df.to_csv('sample.txt')
+    df.to_csv("input/train_folds.csv", index=False)
 
-nbvnbv
+
 
 
